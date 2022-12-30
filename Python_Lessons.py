44ed64518425e0a888e129dc676aa6f6cf05af5a
Nandakumar Ravindran <nandakumartr0905@gmail.com>
2022-12-30 12:19:29 +0800

    Create Python_Lessons.py

---
+++ Python_Lessons.py
@@ -0,0 +1,13 @@
+#// Create a text file in python ? 
+with open("SampleFile.txt", "a") as file:
+    file.write("Saving using Python 1")
+
+
+#'r' open for reading (default)
+#'w' open for writing, truncating the file first
+
+
+#'x' open for exclusive creation, failing if the file already exists
+#'a' open for writing, appending to the end of the file if it exists
+
+
